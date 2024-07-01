45faf35a69427eb11db908401ab53e65cd25c6fe
Agustin Vera <agustin.vera@possumus.tech>
2024-07-01 08:07:17 -0300

    add: vite, typescript and eslint configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "lvlup-project-lib",
+      name: "validateit",
     },
     sourcemap: true,
     target: "es6",
