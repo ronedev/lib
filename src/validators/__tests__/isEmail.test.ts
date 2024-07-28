2f8adb1bf3ef1e640596b23d990ee9be4f8964d7
Agustin Vera <ronedev25@gmail.com>
2024-07-28 18:53:11 -0300

    add: more cases in isEmail test

--- src/validators/__tests__/isEmail.test.ts
+++ src/validators/__tests__/isEmail.test.ts
@@ -4,6 +4,9 @@ describe('isEmail', () => {
   it.each([
     { tested: null, expected: false, label: null },
     { tested: '', expected: false, label: '' },
+    { tested: undefined, expected: false, label: undefined },
+    { tested: 'testcorreo.com', expected: false, label: 'testcorreo.com' },
+    { tested: 'test@correo', expected: false, label: 'test@correo' },
     { tested: 'test@correo.com', expected: true, label: 'test@correo.com' }
   ])('isEmail($label) should be $expected', ({ tested, expected }) => {
     // @ts-expect-error tested can be null in this example
